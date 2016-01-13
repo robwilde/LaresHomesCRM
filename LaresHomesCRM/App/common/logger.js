388653db7f7c7aa67d396f083d4bfdc3839071b1
Robert Wilde <robert@wildetech.com.au>
2016-01-13 23:48:51 +1000

    OK now the logger is working OK

--- LaresHomesCRM/App/common/logger.js
+++ LaresHomesCRM/App/common/logger.js
@@ -46,12 +46,28 @@
             showNotification = showNotification || true;
 
             // write to angular log, & specify error if it is an error
-            var write = (notificationType === 'error') ? $log.error : $log.info;
+            //var write = (notificationType === 'error') ? $log.error : $log.info;
+            switch (notificationType) {
+                case 'info':
+                    var write = $log.info;
+                    break;
+                case 'debug':
+                    var write = $log.debug;
+                    break;
+                case 'error':
+                    var write = $log.error;
+                    break;
+                case 'success':
+                    var write = $log.success;
+                    break;
+                case 'warning':
+                    var write = $log.warning;
+                    break;
+            }
             source = source ? '[' + source + '] ' : '';
             write(source, message, data);
 
             if (showNotification) {
-
                 switch (notificationType) {
                     case 'info':
                         if (!config.showDebugNotiSetting) {
@@ -82,8 +98,7 @@
                         notiTitle = "Lares Homes CRM";
                         break;
                 }
-
-            }
+            };
 
             // create sharepoint notification
             var notificationData = new SPStatusNotificationData("", STSHtmlEncode(message), iconUrl, null);
